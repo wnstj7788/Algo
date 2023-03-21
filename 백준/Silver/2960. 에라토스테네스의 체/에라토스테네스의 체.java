2b6ce6d1a12cfbb684b63cfa19f13279fad765b7
gom <61938768+wnstj7788@users.noreply.github.com>
2023-03-21 12:00:48 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 220 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		// 배열 선언 		
+		
+		int cnt =0;
+		boolean[] arr  = new boolean[n + 1];
+		
+		for(int i=2; i<=n; i++) {
+			for(int j=i; j<=n; j+=i) {	// i배 제거
+				if(!arr[j]) {
+					arr[j] = true;
+					cnt++;
+				}
+				if(cnt == k) {				// j: k번째로 지워진 수
+					System.out.println(j);
+					return;
+				}
+			}
+		}
+		
+		
+		
+		
+		
+	}
+}
