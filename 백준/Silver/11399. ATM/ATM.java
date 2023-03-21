977c43645efac93dd7b6b20c817336e6fb5f54d2
gom <61938768+wnstj7788@users.noreply.github.com>
2023-03-21 12:05:52 +0900

    [Silver IV] Title: ATM, Time: 268 ms, Memory: 20008 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		
+		int[] arr = new int[num];
+		
+		for(int i = 0; i <num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		int total_time = 0;
+		int sum = 0;
+		for(int i =0; i < num; i++) {
+			total_time += arr[i] + sum;
+			sum += arr[i];
+			
+			
+		}
+		//1 2 3 3 4
+		System.out.println(total_time);
+		
+		
+
+		
+
+	}
+
+}
