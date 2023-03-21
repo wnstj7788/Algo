3493e967e58030ca4552e6d6295793fbbb9b6f03
gom <61938768+wnstj7788@users.noreply.github.com>
2023-03-21 11:55:13 +0900

    [Bronze I] Title: 단어 공부, Time: 468 ms, Memory: 29000 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		String k = s.toUpperCase();
+		
+		//ASCII CODE  A = 65
+		
+		int[] arr = new int[26];// 알파벳에 해당하는 배열  
+		
+		for(int i = 0; i < k.length(); i++) {
+			arr[k.charAt(i) - 'A' ]++;
+		}
+		
+		int max = -1;
+		char ch = '?';
+		for(int j = 0; j < arr.length; j++) {
+			if(arr[j] > max) {
+				max = arr[j];
+				ch = (char) (j + 65); 
+			}
+			else if(arr[j] == max) {
+				ch = '?';
+			}
+		}
+		
+		System.out.println(ch);
+	}
+
+}
+	
