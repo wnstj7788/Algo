5850437f8ce967ae6a5bdab05d8ca0080919c1cd
gom <61938768+wnstj7788@users.noreply.github.com>
2023-03-21 12:01:46 +0900

    [Silver V] Title: LCM, Time: 200 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static long GCD(long a, long b) {
+		long temp;
+		if (a < b) {
+			temp = b;
+			b = a;
+			a = temp;
+		}
+		if (b == 0) {
+			return a;
+		}
+		return GCD(b, a % b);
+
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long  n = sc.nextInt();
+		for(long i = 0; i < n ; i++) {
+			long a = sc.nextInt();
+			long b = sc.nextInt();
+			
+			long gcd = GCD(a,b);
+			
+			System.out.println((a * b) / gcd);
+			
+		}
+		
+		
+	}
+
+}
