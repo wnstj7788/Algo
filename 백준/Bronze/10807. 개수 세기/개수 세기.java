c99e2ccec244a5b7f4ec7c2fc1e42cdc9dc6ab12
gom <61938768+wnstj7788@users.noreply.github.com>
2023-03-21 12:04:05 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int arr[] = new int[N];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int M = sc.nextInt();
+		
+		int cnt =0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == M) cnt ++;
+		}
+		
+		System.out.println(cnt);
+		
+	}
+
+}
