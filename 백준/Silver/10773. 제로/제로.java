1fad63d08fd10cbebcc6173bc3a729b0778ea450
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-01-15 21:37:53 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 22840 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -5,26 +5,26 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<>();
 
         int len = Integer.parseInt(br.readLine());
-
         for (int i = 0; i <len ; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if(num == 0){
+            int command = Integer.parseInt(br.readLine());
+
+            if(command == 0){
                 stack.pop();
             }else{
-                stack.push(num);
+                stack.push(command);
             }
         }
-
-        int size = stack.size();
         int result = 0;
-        for (int i = 0; i <size ; i++) {
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
             result += stack.pop();
         }
 
         System.out.println(result);
+
     }
 }
