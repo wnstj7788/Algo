0f01da8d2dfb444f1cb27de0bb6aed8437930754
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-01-15 21:43:44 +0900

    [Silver II] Title: 스택 수열, Time: 344 ms, Memory: 27196 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Stack;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();	// 출력할 결과물 저장
 
@@ -41,7 +41,5 @@ public class Main {
         }
 
         System.out.println(sb);
-
     }
 }
-
