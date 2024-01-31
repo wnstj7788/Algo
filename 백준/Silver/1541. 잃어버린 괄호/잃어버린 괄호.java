a92151bb5cca429e5985f84467913203878ac72b
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-02-01 00:05:24 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,33 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] sub = br.readLine().split("-"); //습관 1 문자를 자를 때 꼭 Stringtokenzier를 사용하려고함
-        // 배열을 길이 개념을 다시 확인해볼 수 있었음
-        int result = 0;
-//        System.out.println(Arrays.toString(sub));
-//        System.out.println(sub.length);
-        for (int i = 0; i <sub.length ; i++) {
-            String[] add = sub[i].split("\\+"); //+를 기준으로 각각의 식을 나눈다
-            int sum = 0;
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
 
-            for(int j = 0; j < add.length; j++){
-                sum += Integer.parseInt(add[j]);
+        int total = Integer.MAX_VALUE;
+        // String[] partition = br.readLine().split("-"); // -를 기준으로 자르기
 
-            }
-            if(i == 0){
-                result += sum;
-            }else{
-                result -= sum;
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+
+        while(st.hasMoreTokens()){
+            int value = 0;
+
+            StringTokenizer st2 = new StringTokenizer(st.nextToken(), "+");
+
+            while(st2.hasMoreTokens()){
+
+               value +=Integer.parseInt(st2.nextToken());
             }
 
+
+
+            if(total == Integer.MAX_VALUE){
+                total = value;
+            } else {
+                total -= value;
+            }
         }
 
-        System.out.println(result);
+        System.out.println(total);
+
+
+
+
     }
 }
