f64dbf166974f6c87842e116c4c2a9f4ad3c4fc3
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-02-05 21:12:46 +0900

    [Silver I] Title: 그림, Time: 436 ms, Memory: 43980 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,4 +1,4 @@
-import java.awt.Point;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,73 +7,67 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int result = Integer.MIN_VALUE;
-	static int map[][];
-	static boolean visited[][];
-	static int dx[] = {-1,0,1,0};
-	static int dy[] = {0,-1,0,1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		visited = new boolean[N][M];
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				
-			}
-		} // end input
-		
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if(map[i][j] == 1 && !visited[i][j]) {
-					bfs(i,j);
-					cnt++;
-				}	
-			}
-		} //start bfs end 
-		
-		System.out.println(cnt);
-		System.out.println(result == Integer.MIN_VALUE ? 0 : result);
-	}
 
-	private static void bfs(int i, int j) {
-		Queue<Point> q = new LinkedList<>();
-		visited[i][j] = true;
-		int drawcnt = 1;
-		
-		q.add(new Point(i,j));
-		while(!q.isEmpty()) {
-			Point now = q.poll();
-			
-			for (int d = 0; d < dx.length; d++) {
-				int nx = now.x + dx[d];
-				int ny = now.y + dy[d];
-				
-				if(nx >=0 && nx  < N && ny >=0 && ny <M) {
-					if(!visited[nx][ny] && map[nx][ny] == 1) {
-						visited[nx][ny] = true;
-						drawcnt ++;
-						
-						q.add(new Point(nx, ny));	
-					}
-				}
-				
-				
-			}//for end
-		}//while end 
-		result = Math.max(result, drawcnt);
-	}//bfs end
+    static int N, M, cnt;
+    static int max = Integer.MIN_VALUE;
+    static int[][] map;
 
+    static int dx[] = {-1, 0, 1, 0};
+    static int dy[] = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int pictureCount = 0;
+        int maxSize = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                    int size = bfs(i, j);
+                    pictureCount++;
+                    maxSize = Math.max(maxSize, size);
+                }
+            }
+        }
+
+        System.out.println(pictureCount);
+        System.out.println(maxSize);
+    }
+
+    public static int bfs(int i, int j) {
+        Queue<Point> q = new LinkedList<>();
+        map[i][j] = 2;
+        q.add(new Point(i, j));
+        int num = 1;
+
+        while (!q.isEmpty()) {
+            Point now = q.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int nextX = now.x + dx[k];
+                int nextY = now.y + dy[k];
+
+                if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && map[nextX][nextY] == 1) {
+                    num++;
+                    map[nextX][nextY] = 2;
+                    q.add(new Point(nextX, nextY));
+                }
+            }
+        }
+
+        return num;
+    }
 }
