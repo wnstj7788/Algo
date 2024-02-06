5e423d72f7062e8d4a3e02598fdd98b177ecb055
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-02-06 22:02:07 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2444 ms, Memory: 439196 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,68 +1,65 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb  = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int TC = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        // initialize treemap<value, count>
+        TreeMap<Integer, Integer> tm = new TreeMap<>(); // 작은 값이 앞에, 큰 값에 뒤에 저장됨
+        //  red-black tree의 방법으로 삽입 시에 바로 정렬이 되는 자료구조
+        //  그래서 삽입 시에 O(log N)의 시간복잡도가 소요
+       // 또한 lastKey()와 firstKey()를 통해 값을 가져오는 연산도 O(log N)이라는 시간복잡도 밖에 소요되지 않는다
 
-        for (int tc = 0; tc  < TC ; tc++) {
-            TreeMap<Integer, Integer> tree = new TreeMap<>();
-            st = new StringTokenizer(br.readLine());
-            int cnt = Integer.parseInt(st.nextToken());
-
-            for (long i = 0; i < cnt; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++){
+            int k = Integer.parseInt(br.readLine());
+            tm.clear();
+            for(int j = 0; j < k; j++){
                 st = new StringTokenizer(br.readLine());
-
-                String command = st.nextToken();
+                char func = st.nextToken().charAt(0);
                 int num = Integer.parseInt(st.nextToken());
-
-                if(command.equals("I")){
-                    // I가 들어있냐 없냐
-                    if(tree.containsKey(num)){
-                        tree.put(num, tree.get(num) +1);
-                    }else{
-                        tree.put(num, 1);
-                    }
-
-                } else if (command.equals("D")) {
-                    if(!tree.isEmpty()){
-                        if(num == -1){
-                            // 최소값 삭제
-                            int min = tree.firstKey();
-                            if(tree.get(min) == 1){
-                                tree.remove(min);
-                            }else{
-                                tree.put(min,tree.get(min) -1);
-                            }
-
-                        } else if (num == 1) {
-                          int max = tree.lastKey();
-                          if(tree.get(max) == 1){
-                              tree.remove(max);
-                          }else {
-                              tree.put(max, tree.get(max) -1 );
-                          }
-                        }
-
-                    }
-
+                // function
+                // insert
+                if(func == 'I'){
+                    // 같은 값이 삽입될 경우, count+1
+                    /*
+                    tm.getOrDefault(num, 0): TreeMap에서 주어진 key(num)에 해당하는 값(value)을 가져오는데, 
+                    만약 key가 존재하지 않으면 기본값으로 0을 사용합니다.+1: 가져온 값에 1을 더합니다.
+                    따라서 전체적으로는 "만약 TreeMap에 이미 해당 key(num)가 존재하면 해당 key의 값을 1 증가시키고, 
+                    존재하지 않으면 해당 key를 1로 설정하여 TreeMap에 추가한다"는 의미를 가지고 있습니다.
+                     */
+                    tm.put(num, tm.getOrDefault(num, 0)+1);
                 }
-
-
+                else{
+                    if(tm.isEmpty()) continue;
+                    int value, cnt;
+                    // delete max
+                    if(num == 1){
+                        value = tm.lastKey();
+                    }
+                    //delete min
+                    else{
+                        value = tm.firstKey();
+                    }
+                    cnt = tm.get(value);
+                    // delete if count == 1
+                    tm.remove(value);
+                    if(cnt > 1) tm.put(value, cnt-1);
+                }
             }
-
-            if(!tree.isEmpty()){
-                sb.append(tree.lastKey() + " ");
-                sb.append(tree.firstKey() + "\n");
-            }else{
-                sb.append("EMPTY" + "\n");
+            // print empty
+            if(tm.isEmpty()) sb.append("EMPTY\n");
+                // print max, min
+            else{
+                sb.append(tm.lastKey()).append(" ").append(tm.firstKey()).append("\n");
             }
-
         }
-
         System.out.println(sb.toString());
-
     }
 }
