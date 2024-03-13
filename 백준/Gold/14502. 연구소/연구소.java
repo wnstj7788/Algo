e6393e34d558fc828e88ca6e376bb7817a4d85a9
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-03-13 10:57:40 +0900

    [Gold IV] Title: 연구소, Time: 700 ms, Memory: 296000 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,168 +1,204 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+ * 인체에 치명적인 바이러스를 연구하던 연구소에서 바이러스가 유출되었다. 다행히 바이러스는 아직 퍼지지 않았고, 바이러스의 확산을 막기 위해서 연구소에 벽을 세우려고 한다.
+
+연구소는 크기가 N×M인 직사각형으로 나타낼 수 있으며, 직사각형은 1×1 크기의 정사각형으로 나누어져 있다. 연구소는 빈 칸, 벽으로 이루어져 있으며, 벽은 칸 하나를 가득 차지한다. 
+
+일부 칸은 바이러스가 존재하며, 이 바이러스는 상하좌우로 인접한 빈 칸으로 모두 퍼져나갈 수 있다. 새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다.
+
+예를 들어, 아래와 같이 연구소가 생긴 경우를 살펴보자.
+
+2 0 0 0 1 1 0
+0 0 1 0 1 2 0
+0 1 1 0 1 0 0
+0 1 0 0 0 0 0
+0 0 0 0 0 1 1
+0 1 0 0 0 0 0
+0 1 0 0 0 0 0
+이때, 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 곳이다. 아무런 벽을 세우지 않는다면, 바이러스는 모든 빈 칸으로 퍼져나갈 수 있다.
+
+2행 1열, 1행 2열, 4행 6열에 벽을 세운다면 지도의 모양은 아래와 같아지게 된다.
+
+2 1 0 0 1 1 0
+1 0 1 0 1 2 0
+0 1 1 0 1 0 0
+0 1 0 0 0 1 0
+0 0 0 0 0 1 1
+0 1 0 0 0 0 0
+0 1 0 0 0 0 0
+바이러스가 퍼진 뒤의 모습은 아래와 같아진다.
+
+2 1 0 0 1 1 2
+1 0 1 0 1 2 2
+0 1 1 0 1 2 2
+0 1 0 0 0 1 2
+0 0 0 0 0 1 1
+0 1 0 0 0 0 0
+0 1 0 0 0 0 0
+벽을 3개 세운 뒤, 바이러스가 퍼질 수 없는 곳을 안전 영역이라고 한다. 위의 지도에서 안전 영역의 크기는 27이다.
+
+연구소의 지도가 주어졌을 때 얻을 수 있는 안전 영역 크기의 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 지도의 세로 크기 N과 가로 크기 M이 주어진다. (3 ≤ N, M ≤ 8)
+
+둘째 줄부터 N개의 줄에 지도의 모양이 주어진다. 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 위치이다. 2의 개수는 2보다 크거나 같고, 10보다 작거나 같은 자연수이다.
+
+빈 칸의 개수는 3개 이상이다.
+
+출력
+첫째 줄에 얻을 수 있는 안전 영역의 최대 크기를 출력한다.
+
+예제 입력 1 
+7 7
+2 0 0 0 1 1 0
+0 0 1 0 1 2 0
+0 1 1 0 1 0 0
+0 1 0 0 0 0 0
+0 0 0 0 0 1 1
+0 1 0 0 0 0 0
+0 1 0 0 0 0 0
+예제 출력 1 
+27
+ */
 public class Main {
-//바이러스는 상하좌우로 퍼저나갈 수 있다 = 4방 탐색
-//새로 세울 수 있는 벽의 개수 3개이고 전부 사용해야함 
-// 0 빈 칸, 1은 벽,  2는 바이러스
-// 벽을 세우고 난 뒤 안전 영역의 최대 값을 구하자 
-// 바이러스는 여러개 일 수 있음 	
-
-// 전체 경우의 수를 조합으로 계산 후 바이러스 확산 시키고  모든 지역을 접근 할 수 없다면 안전구역 카운트
-// 전체 경우에서 제일 큰 안전 구역을 갔는 경우를 출력하자
-
-// 구현 메소드 : dfs, comb, main 
-// static 변수 : map ,N,M 
-
-	static int N, M;
-	static int R = 3; // 세울 수 있는 벽의 계수
+	
+	
+	// 랜덤 3곳을 뽑아서 바이러스가 가장 적은거 찾기 -> 완탐
+	
+	// 클래스 생성 Node
+	
+	// dfs 선택 안선택 만들기 
+	
+	// bfs 돌기
+	
+	// safe 존 확인하기 
+	// deepCopy  
+	
 	static int map[][];
-	static int map_temp[][]; // 원본 배열을 회손하지 않고 여러가지 상황을 비교하기 위한 임시 배열 생성
-
-	static int dx[] = { -1, 1, 0, 0 };
-	static int dy[] = { 0, 0, -1, 1 }; // 4방으로 확산 바이러스의 이동 경로
-
-	static int safecnt = Integer.MIN_VALUE; // 큰 결과와 비교해야하기 때문에 초기 값을 가장 작은 값으로 설정
-
+	static int N, M;
+	static int dx[] = {-1,0,1,0};
+	static int dy[] = {0,-1,0,1};
+	static int max_result = Integer.MIN_VALUE;
+	
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		 N = Integer.parseInt(st.nextToken());
+		 M = Integer.parseInt(st.nextToken());
+		
 		map = new int[N][M];
-		map_temp = new int[N][M];
-
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		map_temp = map; // 원본 배열을 비교를 위한 임시 배열에 저장하기
-
-		// 조합 돌리기
-		comb(0);
-
-		System.out.println(safecnt);
-
+		 
+		
+		dfs(0);
+		
+		System.out.println(max_result);
 	}
+	
+	private static void dfs(int wallCnt) {		
+		if(wallCnt == 3) {
+            bfs();
+            return;
+        }
 
-	// comb를 사용해서 2차원 배열에서 세울 수 있는 경우를 계산
-	// 1.N * M 1차원 배열을 만들어서 전부 저장할까?
-	// 2. 위 방법은 또 연산해줘야해서 불편할 것 같은데 혹시라도 2차원 배열에 한번에 입력 받아서 계산 할 수 있는 방법이 있을까>
-	// 어떻게 해야할까 여기서 부터 생각을 해보자
-	public static void comb(int cnt) {
-		if (cnt == R) {
-			// 벽을 다 세웠다면 바이러스 돌리기
-		//	System.out.println("debug comb call");
-		//	System.out.println(Arrays.deepToString(map_temp));
-			bfs();
-			return;
-		}
-		// 2차원 배열에 한번에 비교하자
+		// 벽일단 세워봐 
+	
+        for(int i=0; i<N; i++) {
+       
+            for(int j=0; j<M; j++) {
+            	
+                if(map[i][j] == 0) {
+                    map[i][j] = 1;
+                         dfs(wallCnt+1);
+                    map[i][j] = 0;
+                }
+            }
+        }
+	}
+	
+	private static void bfs() {
+		Queue<Node> q = new LinkedList<>();
+		//deep copy 
+		int copyMap[][] = deepCopy();
+		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (map_temp[i][j] == 0) { // 해당 구역이 빈 칸이라면 벽을 세울 수 있기 때문에 0인 경우만 선택
-					map_temp[i][j] = 1; // 벽 생성
-					comb(cnt + 1);
-					map_temp[i][j] = 0; // 사용 후 돌려두기
-
+				if(copyMap[i][j] == 2) {
+					q.add(new Node(i,j));
 				}
 			}
-		}
-
-	}
-
-	// 2차원 배열에서 사용할 수 있는 경우의 수를 적어서 표현하면 되는거잖아
-
-	// dfs bfs 둘 중에하나를 만들어서 전부 순회를 해야하네??
-	public static void bfs() {
-		// 큐를 이용해서 풀어야겠지
-		// 새로운 배열을 안만들고 기존 배열을 사용하니까 계속 값이 변함 
-		// 새로운 바이러스 배열 생성
-		int virus_map [][] = new int[N][M];
-		Queue<virus> q = new LinkedList<virus>();
-		//System.out.println("debug bfs call");
-	//	System.out.println(Arrays.deepToString(map_temp));
+		}// vrius 채집완
 		
-		//virus_map = map_temp; 이렇게 넣으면 안된다  
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				virus_map[i][j] = map_temp[i][j];
-			}
-		} 
-		//System.out.println(Arrays.deepToString(virus_map));
-		
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (virus_map[i][j] == 2) {
-					// 큐에 넣어야하는데 좌표 형태로 넣어줘하네?....바이러스 객체 생성
-					//System.out.println("debug viruszone call");
-					q.add(new virus(i, j));
-
-				}
-			}
-		}
-
-		while (!q.isEmpty()) {
-			// 바이러스 퍼트리고 큐에 넣어서 지속적으로 퍼트려 못 퍼질 떄 까지
-			virus v = q.poll(); // 큐에서 첫 값 뽑아와
-			// 4방으로 퍼지기
+		while(!q.isEmpty()) {
+			Node now = q.poll();
+			
 			for (int i = 0; i < 4; i++) {
-				int nx = v.x + dx[i];
-				int ny = v.y + dy[i];
-				// 범위를 넘지 않고 해당 자리에 벽이 아닌 경우 확장
-				// 0이상이고 N 이하이고 0이상이고 M 이하이고 1
-				if (0 <= nx && 0 <= ny && nx < N && ny < M ) {
-					// 여기서 안멈추네....??
-					// System.out.println("debug 4-way No problem call");
-					if (virus_map[nx][ny] == 0) {
-							virus_map[nx][ny] = 2;
-							q.add(new virus(nx, ny)); // 큐에 넣어줘서 지속 반복
-					}
+				int nextX = now.x + dx[i];
+				int nextY = now.y + dy[i];
+				if(nextX < N && nextX >= 0 && nextY < M && nextY >= 0 && copyMap[nextX][nextY] == 0) {
+					//정상 범위이면서 해당 값이 0일때 
+					copyMap[nextX][nextY] = 2;
+					
+					
+					q.add(new Node(nextX, nextY));
 				}
-
 			}
-
-		}
-		// 비교가 끝났으면 안전 구역 확인해야하는데 메서드를 제작해서 관리
-		safeZone(virus_map);
+		}// 탐색 완 
+		
+		 safeZone(copyMap);
+		
+		
 	}
-
-	static void safeZone(int[][] virus_map) {
-		int cnt = 0;
-		//System.out.println("debug safezone call");
-		//System.out.println(Arrays.deepToString(virus_map));
+	
+	private static int[][] deepCopy() {
+		int newMap[][] = new int[N][M];
+		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if(virus_map[i][j] == 0) { //해당 구역이 안전 구역인 경우 카운트  
+				newMap[i][j] = map[i][j];
+			}
+		}
+		
+		return newMap;
+	}
+	
+	private static void safeZone(int copyMap[][]) {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(copyMap[i][j] == 0) {
+					//안전하면 ++
 					cnt++;
 				}
-				
 			}
-		}
-		safecnt = Math.max(cnt, safecnt);
+		}//end
+		
+		max_result = Math.max(max_result, cnt);
+		
 	}
-
-	static class virus {
-		int x, y;
-
-		virus(int x, int y) {
+	
+	private static class Node{
+		int x;
+		int y;
+		
+		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
-
 	}
 
 }
