f1f81c2e250bf379f0e7dda32951fdf835d83dfc
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-03-25 16:33:41 +0900

    [Silver III] Title: N과 M (3), Time: 516 ms, Memory: 265424 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,39 +1,91 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
 public class Main {
 	
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		M = in.nextInt();
- 
-		arr = new int[M];
-        
-		dfs(0);
-		System.out.print(sb);
- 
+	static int arr[];
+	static boolean visited[];
+	static StringBuilder sb = new StringBuilder();
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());	
+		arr = new int[M]; 
+		visited = new boolean[N];
+		
+		dfs(0,M,N);
+		
+		System.out.println(sb.toString());
+		
 	}
- 
-	public static void dfs(int depth) {
-		// 깊이가 M이랑 같아지면 출력후 return
-		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(arr[i] + " ");
+	
+	
+	
+	public static void dfs(int depth, int M , int N ) {
+		if(depth == M) {
+			for (int temp : arr) {
+				sb.append(temp + " ");
 			}
-			sb.append('\n');
+			sb.append("\n");
 			return;
 		}
- 
-		for (int i = 1; i <= N; i++) {
-			arr[depth] = i;
-			dfs(depth + 1);
+		
+		for (int i = 0; i < N; i++) {
+			if(!visited[i]) {
+				arr[depth] = i +1;
+				if(depth == M -1) {
+					visited[i] = true;
+				}
+				dfs(depth + 1, M, N);
+				
+				if(depth == M -1) {
+					visited[i] = false;
+				}
+				
+			}
+			
 		}
+		
 	}
- 
+	
+
 }
+
+/*
+ * 
+ *
+ * 문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ 1 1
+1 2
+1 3
+1 4
+2 1
+2 2
+2 3
+2 4
+3 1
+3 2
+3 3
+3 4
+4 1
+4 2
+4 3
+4 4
+*/
