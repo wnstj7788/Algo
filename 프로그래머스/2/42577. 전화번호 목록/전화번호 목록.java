b8632fc4248efc3eaa7a726a909c76bd8dff77ac
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-08 08:38:56 +0900

    [level 2] Title: 전화번호 목록, Time: 188.55 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,17 +3,16 @@ import java.util.Map;
 
 class Solution {
     public boolean solution(String[] phoneBook) {
-        // 1. HashMap을 선언한다.
-        Map<String, Integer> map = new HashMap<>();
 
-        // 2. 모든 전화번호를 HashMap에 넣는다.
+        HashMap<String, Integer> hm = new HashMap<>();
+
+
         for (int i = 0; i < phoneBook.length; i++) 
-            map.put(phoneBook[i], i);
+            hm.put(phoneBook[i], i);
         
-        // 3. 모든 전화번호의 substring이 HashMap에 존재하는지 확인한다.
         for (int i = 0; i < phoneBook.length; i++)
             for (int j = 0; j < phoneBook[i].length(); j++)
-                if (map.containsKey(phoneBook[i].substring(0, j)))
+                if (hm.containsKey(phoneBook[i].substring(0, j)))
                     return false;
 
         return true;
