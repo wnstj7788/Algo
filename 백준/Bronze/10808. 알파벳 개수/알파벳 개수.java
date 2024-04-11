c5eada6597aaeb83299cbdd9a3d18961e50c3d04
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-11 22:09:55 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,30 +1,27 @@
-import java.util.*;
 import java.io.*;
 
-/*
- String 형으로 문자에 a-z 각각의 알파벳이 몇번 등장하는지 반환
- step1. a - z 배열 선언
- step2. 각각의 유니코드 값 도출 해당 인덱스 + 1
- stop3. 반환
- */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] st = br.readLine().toCharArray();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String temp = br.readLine();
+		
+		
+		int arr[] = new int[26];
+		
+		char[] tempArr = temp.toCharArray();
+		
+		
+		for(int i =0; i < tempArr.length; i++) {
+			arr[(tempArr[i] - 97)]++;
+		}
+		
+		for(int i =0; i < 26; i++) {
+			System.out.print(arr[i] + " ");
+		}
+				
+		
+		
+	}
 
-        int result[] = new int[26];
-
-
-        for (char c : st) {
-           // int index = c - 97; //  a
-            int index = c -'a';
-            result[index]++;
-        }
-
-        for (int now : result){
-            System.out.print(now + " ");
-        }
-
-
-    }
 }
