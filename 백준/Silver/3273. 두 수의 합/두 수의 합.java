b0e0e044d025235c0f750c7c38684eef000af342
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-12 21:28:52 +0900

    [Silver III] Title: 두 수의 합, Time: 228 ms, Memory: 25396 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,37 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i =0; i < N; i ++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		int x = Integer.parseInt(br.readLine());
+		int result[] = new int[x + 1];
+		
+		
+		int answer = 0;
+		
+		// result 1 2 3 4 5 6 7 8 9 10 11 12 13
+		// arr [5 12 7 10 9  1 2 3  11] 
+		
+		for (int i = 0; i < N; i++) {
+			if((x- arr[i]) >= 0 && result[x - arr[i]] == 1) {
+				
+				answer++;
+			}else if(arr[i] <= x){
+				result[arr[i]] = 1;
+			}
+		}
+		
+		System.out.println(answer);
+		
+		
+		
+	}
+	
+	// 13 - 12 = 1
 
-    //n개의 서로 다른 양의 정수 a1, a2, ..., an으로 이루어진 수열이 있다.
-    // ai의 값은 1보다 크거나 같고, 1000000보다 작거나 같은 자연수이다.
-    // 자연수 x가 주어졌을 때, ai + aj = x (1 ≤ i < j ≤ n)을 만족하는 (ai, aj)쌍의 수를 구하는 프로그램을 작성하시오.
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int len = Integer.parseInt(br.readLine());
-        int arr[] = new int[len];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int target = Integer.parseInt(br.readLine());
-        for (int i = 0; i <len ; i++) {
-           arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-        int answer = 0;
-        for (int i = 0; i <len ; i++) {
-            for (int j = i + 1; j <len ; j++) {
-               if(target == arr[i] + arr[j]){
-                   answer++;
-                   break;
-               }
-            }
-        }
-
-        System.out.println(answer);
-    }
 }
