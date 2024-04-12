31ac860a567b024286b87fe5392a07d4daeebf9a
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-12 17:23:16 +0900

    [Silver V] Title: 방 번호, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -2,48 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*
-1 - 9까지 1set = 최소 set을 구해라
-6 - 9 는 뒤집어서 사용할 수 있다.
-
-step1. 1-9 배열 생성 초기 값 0
-step2. 현재값이 6인지 9인지 확인
-
- */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String temp = br.readLine();
+		int cnt = 0;
+		
+		// 9 랑 6을 똑같음 
+		//2개까지는 똑같음 //2 하면 될 듯
 
-        int size = str.length();
-        int[] arr = new int[10];
-        int temp = 0;
-        int max = 0;
+		
+		int arr[]= new int[10];
+		
+		for (int i = 0; i < temp.length(); i++) {
+			char tempChar = temp.charAt(i);
+			
+			if(tempChar - '0' == 6) {
+				arr[9] ++;
+				
+			}else {
+				arr[tempChar - '0']++;
+			}
+		}
+		
+		int result = 0;
+		
+		arr[9] = arr[9]/ 2 + arr[9]%2;
+		
+		
+		
+		for (int i = 0; i < arr.length; i++) {
+			result = Math.max(arr[i], result);
+		}
+		System.out.println(result);
+		
+		
+		 
+		
+		
+		
+		
+	}
 
-        //counting 배열을 사용하였다.
-        for (int i = 0; i < size; i++) {
-            temp = str.charAt(i) - '0';
-            arr[temp]++;
-
-        }
-
-
-        int k = (arr[6] + arr[9]);
-        //6,9의 합이 짝수이면 반으로 나누면 된다.
-        if (k % 2 == 0) {
-            arr[6] = k / 2;
-            arr[9] = k / 2;
-        } else {
-            arr[6] = k / 2 + 1;
-            arr[9] = k / 2 + 1;
-        }
-        //반복해서 최대값을 찾는다.
-        for (int i : arr) {
-            max = Math.max(max, i);
-        }
-        System.out.println(max);
-
-
-
-    }
 }
