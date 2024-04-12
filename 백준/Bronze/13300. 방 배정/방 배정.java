2c5edc3de7a910d461e5393788136b149b7fc2ab
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-12 21:59:47 +0900

    [Bronze II] Title: 방 배정, Time: 76 ms, Memory: 11524 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int map[][] = new int[2][6];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			map[s][y - 1]++; 
+		}
+		
+		
+		int result = 0;
+		
+		for(int i =0; i < 2; i++) {
+			for(int j = 0; j < 6; j++) {
+				if(map[i][j] == 0) {
+					
+				}else {
+					if(map[i][j] < M) {
+						result++;
+					}else {
+						if(map[i][j] / M !=0 ) {
+
+							result = result + map[i][j] / 3 + 1;  
+						}
+						else {
+							result += map[i][j] / 3;
+						}
+					}
+				}
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
