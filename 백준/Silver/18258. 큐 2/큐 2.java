6bded6218a865157cbfd9791e6a0821b5e3da2db
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-14 18:48:39 +0900

    [Silver IV] Title: 큐 2, Time: 1228 ms, Memory: 342532 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,83 +1,61 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.Deque;
-import java.util.ArrayDeque;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		Deque<Integer> q = new ArrayDeque<>();
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		StringTokenizer command;
-		
-		while(N-- > 0) {
-			command = new StringTokenizer(br.readLine(), " ");	// 문자열 분리 
-			
-			switch(command.nextToken()) {	
-			
-			case "push":
-				// offer는 큐의 맨 뒤에 요소를 추가한다.
-				q.offer(Integer.parseInt(command.nextToken()));	
-				break;
-				
-			case "pop" :
-				/*
-				 *  poll은 가장 앞에 있는 요소를 삭제하며
-				 *  삭제할 원소가 없을 경우 예외를 던지는 것이 아닌 null을 반환한다.
-				 */
-				Integer item = q.poll();	
-				if(item == null) {
-					sb.append(-1).append('\n');
-				}
-				else {
-					sb.append(item).append('\n');
-				}
-				break;
-				
-			case "size":	
-				sb.append(q.size()).append('\n');
-				break;
-				
-			case "empty":
-				if(q.isEmpty()) {
-					sb.append(1).append('\n');
-				}
-				else {
-					sb.append(0).append('\n');
-				}
-				break;
-				
-			case "front":
-				// peek()은 큐에 꺼낼 요소가 없을 경우 null을 반환한다.
-				Integer ite = q.peek();
-				if(ite == null) {
-					sb.append(-1).append('\n');
-				}
-				else {
-					sb.append(ite).append('\n');
-				}
-				break;
-				
-			case "back":
-				// peekLast()은 큐에 꺼낼 요소가 없을 경우 null을 반환한다.
-				Integer it = q.peekLast();	 
-				if(it == null) {
-					sb.append(-1).append('\n');
-				}
-				else {
-					sb.append(it).append('\n');
-				}
-				break;
-			}
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+        int last = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            
+            if("push".equals(command)){
+                int x = Integer.parseInt(st.nextToken());
+                last = x;
+                q.add(x);
+
+            }else if("pop".equals(command)){
+                if(!q.isEmpty()){
+                    sb.append(q.poll() + "\n");
+                }else{
+                    sb.append(-1 + "\n");
+                }
+            }else if("size".equals(command)){
+                sb.append(q.size() + "\n");
+                
+            }else if("empty".equals(command)){
+                if(!q.isEmpty()){
+                    sb.append(0 + "\n");
+                }else{
+                    sb.append(1 + "\n");
+                }
+                
+            }else if("front".equals(command)){
+                if(!q.isEmpty()){
+                    sb.append(q.peek()+ "\n");
+                }else{
+                    sb.append(-1 + "\n");
+                }
+                
+            }else if("back".equals(command)){
+                if(!q.isEmpty()){
+                    sb.append(last+ "\n");
+                }else{
+                    sb.append(-1 + "\n");
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
