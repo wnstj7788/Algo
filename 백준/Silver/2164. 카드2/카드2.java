ee6eadb90314ddcfc71344954512536f48c9bf79
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-14 18:55:43 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 51128 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,26 @@
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
- 
-import java.util.Scanner;
- 
+import java.util.Queue;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		Queue<Integer> q = new LinkedList<>();
-		
-		int N = in.nextInt();
-		
-		for(int i = 1; i <= N; i++) {
-			q.offer(i);
-		}
-		
-		
-		while(q.size() > 1) {
-			q.poll();	// 맨 앞의 원소 버림 
-			q.offer(q.poll());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입 
-		}
-		
-		System.out.println(q.poll());	// 마지막으로 남은 원소 출력 
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Queue<Integer> q = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+      while(q.size() != 1){
+            int y= q.poll();
+            // System.out.println("버린카드 " + y);
+            int x = q.poll();
+            // System.out.println("넣은카드 " + x);
+            q.add(x);
+      }
+      System.out.println(q.poll());
+    }
 }
