0a790c8af530e805cc7656b5196e6e93ebe7b290
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-22 20:30:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 120 ms, Memory: 17152 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,54 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N;
-    static int K;
-    static int visit[] = new int[100001];
-
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        K = sc.nextInt();
-
+    static int N, K;
+    static int dx[] = {1,-1,2};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         bfs();
-        if(K == 0) {
-        	System.out.println(N);
-        }else
-        	System.out.println(visit[K]);
+        
+    }
 
+    public static void bfs(){
+        Queue<Node> q = new LinkedList<>();
+        boolean visited[] = new  boolean[100001];
+
+        q.add(new Node(N,0));
+        visited[N] = true;
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+
+            if(now.x == K){
+                System.out.println(now.cnt);
+                break;
+            }
+
+            for (int i = 0; i < dx.length; i++) {
+                int nx = 0;
+                if(i == 2){
+                     nx = now.x * 2;
+                }else{
+                     nx = now.x + dx[i];
+                }
+
+                if(nx >= 0 && nx <= 100000 && !visited[nx]){
+                    q.add(new Node(nx, now.cnt+1));
+                    visited[nx] = true;
+                }
+            }
+
+        }
+        
+    }
+
+
+    public static class Node{
+        int x;
+        int cnt;
+
+        public Node(int x, int cnt){
+            this.x = x;
+            this.cnt = cnt;
+        }
     }
     
-    private static void bfs(){
-    	
-    	Queue<Integer> q = new LinkedList<>();
-    	q.offer(N);
-    	
-    	while (!q.isEmpty()){
-    		int tmp = q.poll();
-    		if(tmp==K){
-    			break;
-    		}
-    		for(int i=0; i<3; i++){
-    			int next;
-    			
-    			if (i == 0) {
-    				next = tmp + 1;
-    			} else if (i == 1) {
-    				next = tmp - 1;
-    			} else {
-    				next =tmp * 2;
-    			}
-    			if (next >= 0 && next < visit.length && visit[next] == 0) {
-    				q.offer(next);
-    				visit[next] = visit[tmp] + 1;
-    			}
-    		}
-    	}
-    }
 }
