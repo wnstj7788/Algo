03fdf7cad42ae71bb3e455d16feb1d550a7d64ce
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-23 12:33:09 +0900

    [Gold V] Title: 적록색약, Time: 104 ms, Memory: 13064 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,136 +1,107 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-	public static class RGB {
-		int color;
-		int x;
-		int y;
 
-		public RGB(int color, int x, int y) {
-			super();
-			this.color = color;
-			this.x = x;
-			this.y = y;
-		}
+    static int N;
+    static int[] xArr = {-1, 1, 0, 0};
+    static int[] yArr = {0, 0, -1, 1};
+    static char[][] RGB;
+    static char[][] RGB2;
+    static boolean[][] visited;
+    static int[] RGBCheck = new int[3];
 
-	}
-	static boolean[][] vistied;
-	static int N;
-	static int map[][];
-	static int dx[] = { -1, 0, 1, 0 };
-	static int dy[] = { 0, -1, 0, 1 };
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-		N = Integer.parseInt(br.readLine());
+        RGB = new char[N][N];
+        RGB2 = new char[N][N];
+        visited = new boolean[N][N];
 
-		map = new int[N][N];
-		vistied = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			String temp = br.readLine();
-			for (int j = 0; j < N; j++) {
-				map[i][j] = temp.charAt(j) - 'A';
-			}
-		} // end input
+        //적록색약을 위한 RGB2 생성. insert 시 한번에
+        for (int i = 0; i < N; i ++) {
+            String color = br.readLine();
+            for (int j = 0; j < N; j ++) {
+                RGB[i][j] = color.charAt(j);
 
-		int cnt = 0;
+                if(color.charAt(j) == 'G') {
+                    RGB2[i][j] = 'R';
+                }
+                else RGB2[i][j] = color.charAt(j);
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {				
-				if(!vistied[i][j])
-					cnt++;
-					bfs(new RGB(map[i][j], i, j));
+        //적록색약이 아닌 사람의 bfs
+        for (int i = 0; i < N; i ++) {
+            for (int j = 0; j < N; j ++) {
+                if(!visited[i][j]) bfs(RGB[i][j], i, j, "RGB");
+            }
+        }
+        
+        int answer1 = RGBCheck[0] + RGBCheck[1] + RGBCheck[2];
+        
+        //적록색약을 위한 초기화
+        RGBCheck = new int[3];
+        visited = new boolean[N][N];
 
-			}
-		}
+        //적록색약의 bfs
+        for (int i = 0; i < N; i ++) {
+            for (int j = 0; j < N; j ++) {
+                if(!visited[i][j]) bfs(RGB2[i][j], i, j, "RGB2");
+            }
+        }
 
-		System.out.println(cnt);
-		
-		
-		// 적록 색약인 경우 다시 G-> R로 만들고 실행하자
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(map[i][j] == 6) {
-					map[i][j] = 17;
-				}
-				
-			}
-		}
-		// 방문배열 초기화 
-		vistied = new boolean[N][N];
-		int cnt_false = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {				
-				if(!vistied[i][j])
-					cnt_false++;
-					bfs(new RGB(map[i][j], i, j));
+        int answer2 = RGBCheck[0] + RGBCheck[1] + RGBCheck[2];
+        System.out.println(answer1 + " " + answer2);
 
-			}
-		}
-		System.out.println(cnt_false);
-		
-		
-		
-		
-		
-		
-		
+    }
 
-	}// end main
+    private static void bfs(char color, int y, int x, String tmp) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{y, x});
+        visited[y][x] = true;
 
-	private static void bfs(RGB rgb) {
-		// TODO Auto-generated method stub
-	
-		Queue<RGB> q = new LinkedList<>();
-		q.add(new RGB(rgb.color, rgb.x, rgb.y));
-		int target = rgb.color;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
 
-		
-		vistied[rgb.x][rgb.y] = true;
+            for (int i = 0; i < size; i ++) {
+                int[] current = queue.poll();
+                x = current[1];
+                y = current[0];
 
-	//	map[rgb.x][rgb.y] = 0;
-			
-		
-		while (!q.isEmpty()) {
-			RGB now = q.poll();
+                for (int j = 0; j < 4; j ++) {
+                    int newX = x + xArr[j];
+                    int newY = y + yArr[j];
 
-			if (now.color != target) {
-				return;
-			}
-			
-			for (int d = 0; d < 4; d++) {
-				int nx = now.x + dx[d];
-				int ny = now.y + dy[d];
-
-				if (nx >= 0 && nx < N && ny >= 0 && ny < N && !vistied[nx][ny] && map[nx][ny] == target) {
-					
-					q.add(new RGB(target, nx, ny));
-					vistied[nx][ny] = true;
-					//map[nx][ny] = 0;
-					//print(map);
-					
-				}
-			}
-
-		}
-
-	}
-
-	private static void print(int arr[][]) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println("----------------------");
-	}
-
+                    if(newY < 0 || newY >= N || newX < 0 || newX >= N || visited[newY][newX]) continue;
+                    if(tmp.equals("RGB")) {
+                        if(RGB[newY][newX] == RGB[y][x]) {
+                            queue.add(new int[]{newY, newX});
+                            visited[newY][newX] = true;
+                        }
+                    }
+                    else {
+                        if(RGB2[newY][newX] == RGB2[y][x]) {
+                            queue.add(new int[]{newY, newX});
+                            visited[newY][newX] = true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        //처음 들어왔던 color을 활용하여 어떤 color을 탐색했는지 식별
+        switch(color) {
+            case 'R': RGBCheck[0]++;
+                break;
+            case 'G': RGBCheck[1]++;
+                break;
+            case 'B': RGBCheck[2]++;
+                break;
+        }
+    }
 }
