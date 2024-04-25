bde918b10a917e48307729cd0fa225622ab87e37
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-04-25 13:21:27 +0900

    [Silver I] Title: 나이트의 이동, Time: 280 ms, Memory: 72316 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,89 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
+/**
+ * BOJ_7562
+ */
 public class Main {
-	static class Point {
-		int x;
-		int y;
-		int cnt;
+    static int dx[] = {1,1,-1,-1,2,2,-2,-2};
+    static int dy[] = {2,-2,2,-2,1,-1,1,-1};
+    static int map[][];
+    static boolean visited[][];
+    static int N, cnt, targetX, targetY;
+    static Queue<Node> q;
+    static StringBuilder sb = new StringBuilder();
+    
 
-		public Point(int x, int y, int cnt) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-		}
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	static int map[][];
-	static boolean visited[][];
-	static int N;
-	static int dx[] = { -2, -2, 2, 2, 1, 1, -1, -1 };
-	static int dy[] = { 1, -1, 1, -1, 2, -2, 2, -2 };
-	static int endI, endJ;
+        int TC = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int TC = sc.nextInt();
+        for (int tc = 0; tc < TC; tc++) {
+            q = new LinkedList<>();
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            visited = new boolean[N][N];
 
-		for (int tc = 0; tc < TC; tc++) {
-			N = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
 
-			map = new int[N][N];
-			visited = new boolean[N][N];
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            q.add(new Node(x,y));
+            
+            map[x][y] = 1;
+            visited[x][y] = true;
 
-			int startI = sc.nextInt();
-			int startJ = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
 
-			map[startI][startJ] = 1;// 시작점
+            targetX = Integer.parseInt(st.nextToken());
+            targetY = Integer.parseInt(st.nextToken());
 
-			 endI = sc.nextInt();
-			 endJ = sc.nextInt();
-			map[endI][endJ] = 2; // 도착점
+            bfs();
+            
+        }
 
-			bfs(startI, startJ, 0);
-		}
+        System.out.println(sb.toString());
+         
+    }
 
-	}
+    public static void bfs(){
+        int result  = 0;
 
-	private static void bfs(int startI, int startJ, int cnt) {
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(startI, startJ, cnt));
-		visited[startI][startJ] = true;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for (int s = 0; s < size; s++) {
+                Node now = q.poll();
+                if(now.x == targetX && now.y == targetY){
+                    sb.append(result + "\n");
+                    return;
+                }
+                    
 
-		while (!q.isEmpty()) {
-			Point now = q.poll();
-			if (now.x == endI && now.y == endJ) {
-				System.out.println(now.cnt);
-				return;
-			}
+                for (int i = 0; i < 8; i++) {
+                    int nx = now.x + dx[i];
+                    int ny = now.y + dy[i];
+                    
+                    if(nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        q.add(new Node(nx,ny));
+                    }
+                }
+            }
+            result++;
+        }
 
-			for (int d = 0; d < dx.length; d++) {
-				int nx = now.x + dx[d];
-				int ny = now.y + dy[d];
+    }
 
-				if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
+    public static class Node{
+        int x;
+        int y;
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-					map[nx][ny] = 1;
-					map[now.x][now.y] = 0;
-					visited[nx][ny] = true;
-					q.add(new Point(nx, ny, now.cnt + 1));
-
-				}
-				//print();
-			}
-		}
-
-	}
-
-	private static void print() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println("---------------------------------------");
-	}
 }
