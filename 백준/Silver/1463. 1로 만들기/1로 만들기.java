6b8fc1278ef12ef1d4b738716b4ccb2df6408dfa
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-05-07 15:10:12 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15492 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,78 +1,29 @@
-import java.awt.Point;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	static int result = Integer.MAX_VALUE;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-		System.out.println(bfs(n));
 
-//		for (int i = 2; i <= n; i++) {
-//			dp[i] = dp[i - 1] + 1; // 1로 뻬기
-//
-//			if (i % 3 == 0) // 3으로 나누어 떨어진다면
-//				dp[i] = Math.min(dp[i], dp[i / 3] + 1) ;
-//			if (i % 2 == 0) //2로 나누어 떨어진다면 
-//				dp[i] = Math.min(dp[i], dp[i / 2] + 1) ;
-//			
-//
-//		}
-//		
-////		System.out.println(Arrays.toString(dp));
-//		System.out.println(dp[n]);
-	}
+        int dp[] = new int[N+1];
 
-	private static int bfs(int n) {
-		Queue<Point> q = new LinkedList<>();
-		boolean visited[] = new boolean[1000001];
-		q.add(new Point(n, 0));
+        dp[0] = 0;
+        dp[1] = 0;
 
-		while (!q.isEmpty()) {
-			Point now = q.poll();
-			int nx = now.x;
-			if (nx < 1) {
-				break;
-			}
-			if (!visited[nx]) {
-				visited[nx] = true;
-				if (nx % 3 == 0) {
-					q.add(new Point(nx / 3, now.y + 1));
-				
-				}
-				if (nx % 2 == 0) {
-					q.add(new Point(nx / 2, now.y + 1));
-			
-				}
-				q.add(new Point(nx - 1, now.y + 1));
-			
 
-//				System.out.println("나 여기 들어옴 n:" + nx + "conunt" + now.y);
-				if (nx == 1) {
-					result = Math.min(result, now.y);
-				}
-			}
-		}
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+        }
 
-		return result;
-	}
+        System.out.println(dp[N]);
 
-	public static class Point {
-		int x;
-		int y;
 
-		public Point(int x, int y) {
-
-			this.x = x;
-			this.y = y;
-		}
-
-	}
-
+    }
 }
