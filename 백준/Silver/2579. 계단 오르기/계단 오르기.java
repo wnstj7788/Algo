0dec86bb8fdf8dae982d427bcf87d1bdf02d6a83
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-05-09 13:35:10 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,40 +1,27 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-  
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
- 
-		int[] dp = new int[N + 1];
-		int[] map = new int[N + 1];
- 
- 
-		for (int i = 1; i <= N; i++) {
-			map[i] =in.nextInt();
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N + 1];
+
+        for(int i = 1; i <= N ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int dp[] = new int[N + 1];
+        dp[1] = arr[1];
+        if (N > 1) dp[2] = arr[1] + arr[2]; 
+
+        for (int i = 3; i <= N ; i++) {
+            // 한 번에 1칸 혹은 2칸을 오를 수 있으므로, 현재 위치에서 한 칸 혹은 두 칸을 오를 때의 최댓값을 비교
+            dp[i] = Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+        }
         
-		
-		//핵심 아이디어 
-		//도착 할 수 있는 경우  
-		// 1. 전칸에서 +1로 올라는 경우 
-		// 3칸 연속으로 못 올라온다는 조건으로 인해 n-3에서(지금까지 더 해진 비용) 2칸 올라오고 n-1(이전 칸은 따로 더하기) 에서 올라오는 수 + 현재칸 
-		//2. 전칸에서 +2로 올라오는 경우
-		// 2칸(지금까지 더 해진 비용)+ 현재칸 
-		dp[1] = map[1];
-        
-		
-		if (N >= 2) {
-			dp[2] = map[1] + map[2];
-		}
- 
-		for (int i = 3; i <= N; i++) {
-			dp[i] = Math.max(dp[i - 2] , dp[i - 3] + map[i - 1]) + map[i];
-		}
- 
-		System.out.println(dp[N]);
- 
-	}
- 
+        System.out.println(dp[N]);
+    }
 }
