b3b0d6fc8be7615bbd7a4b97b9cc7e1883cddb81
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-05-28 13:35:51 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,33 +1,33 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt(); int k = sc.nextInt();
-		
-		int [] arr = new int[n];
-		
-		for(int i = 0; i < n; i++) {
-			arr[i] =sc.nextInt();
-		}
-		
-		int cnt = 0;
-		// 0이 될 때 까지
-		// 마지막 요소로 나누어 진다면 k를 마지막 요소로 나누고 cnt 추가 및 나머지 값 저장
-		// 나머지 값을 다음 요소로 나누고 ...... 0이 될 떄까지 
-	
-		for(int j = n-1 ; j >= 0; j--) {
-			if( k >= arr[j]) {
-				cnt += (k /arr[j]);
-				k = k % arr[j];
-				}
-		
-				
-			}
-		System.out.println(cnt);
-		}
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
 
-	}
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int cnt = 0;
 
+
+        for (int i = N -1; i >= 0; i--) {
+           // System.out.println("1");
+            if( K >= arr[i]){
+                cnt += K / arr[i];
+                K = K % arr[i];
+            }
+        }
+
+        System.out.println(cnt);
+
+
+    }
+}
