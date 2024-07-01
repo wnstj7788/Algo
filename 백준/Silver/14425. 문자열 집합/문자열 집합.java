5bcf9dc502ff73769d27d8b84ea9ebe7df79493c
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-07-01 20:42:12 +0900

    [Silver IV] Title: 문자열 집합, Time: 292 ms, Memory: 39016 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,77 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node{
-
-        Map<Character,Node> childNode = new HashMap<>();
-
-        boolean isEnd;
-    }
-
-    static class Trie{
-
-        Node root = new Node();
-        void insert(String str) {
-
-            Node node = this.root;
-
-            int size = str.length();
-
-            for (int i = 0; i < size; i++) {
-                node = node.childNode.computeIfAbsent(str.charAt(i), key -> new Node());
-            }
-
-            node.isEnd = true;
-        }
-
-        boolean search(String str){
-            Node node = this.root;
-
-            int size = str.length();
-
-            for (int i = 0; i < size; i++) {
-                node = node.childNode.getOrDefault(str.charAt(i),null);
-
-                if(node == null){
-                    return false;
-                }
-            }
-
-        return node.isEnd;
-        }
-
-    }
-
-
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Trie trie = new Trie();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
-        int M  = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        int totalS =0;
+
+        HashSet<String> hs = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            String temp = br.readLine();
-            trie.insert(temp);
+            hs.add(br.readLine());
         }
-
-        int cnt = 0;
 
         for (int i = 0; i < M; i++) {
-            String temp = br.readLine();
-            if(trie.search(temp)){
-                cnt++;
+            String word = br.readLine();
+            if(hs.contains(word)){
+                totalS++;
             }
-
         }
-        System.out.println(cnt);
+
+        System.out.println(totalS);
     }
+    
 }
