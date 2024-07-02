3d7fd3ae09f443bfd83def5413e5fad2652f771a
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-07-02 09:53:12 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2340 ms, Memory: 440132 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,65 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    static int t;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        // initialize treemap<value, count>
-        TreeMap<Integer, Integer> tm = new TreeMap<>(); // 작은 값이 앞에, 큰 값에 뒤에 저장됨
-        //  red-black tree의 방법으로 삽입 시에 바로 정렬이 되는 자료구조
-        //  그래서 삽입 시에 O(log N)의 시간복잡도가 소요
-       // 또한 lastKey()와 firstKey()를 통해 값을 가져오는 연산도 O(log N)이라는 시간복잡도 밖에 소요되지 않는다
-
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++){
-            int k = Integer.parseInt(br.readLine());
-            tm.clear();
-            for(int j = 0; j < k; j++){
-                st = new StringTokenizer(br.readLine());
-                char func = st.nextToken().charAt(0);
-                int num = Integer.parseInt(st.nextToken());
-                // function
-                // insert
-                if(func == 'I'){
-                    // 같은 값이 삽입될 경우, count+1
-                    /*
-                    tm.getOrDefault(num, 0): TreeMap에서 주어진 key(num)에 해당하는 값(value)을 가져오는데, 
-                    만약 key가 존재하지 않으면 기본값으로 0을 사용합니다.+1: 가져온 값에 1을 더합니다.
-                    따라서 전체적으로는 "만약 TreeMap에 이미 해당 key(num)가 존재하면 해당 key의 값을 1 증가시키고, 
-                    존재하지 않으면 해당 key를 1로 설정하여 TreeMap에 추가한다"는 의미를 가지고 있습니다.
-                     */
+        t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> tm = new TreeMap<>();
+            for(int i=0; i<n; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                char order = st.nextToken().charAt(0);
+                if(order=='I'){
+                    int num = Integer.parseInt(st.nextToken());
                     tm.put(num, tm.getOrDefault(num, 0)+1);
                 }
-                else{
-                    if(tm.isEmpty()) continue;
-                    int value, cnt;
-                    // delete max
-                    if(num == 1){
-                        value = tm.lastKey();
+                else if(!tm.isEmpty()){
+                    int option = Integer.parseInt(st.nextToken());
+                    if(option==1){
+                        //최댓값 삭제
+                        int num = tm.lastKey();
+                        if(tm.get(num)==1){
+                            tm.remove(num);
+                        }
+                        else{
+                            tm.put(num, tm.get(num)-1);
+                        }
                     }
-                    //delete min
                     else{
-                        value = tm.firstKey();
+                        //최솟값 삭제
+                        int num = tm.firstKey();
+                        if(tm.get(num)==1){
+                            tm.remove(num);
+                        }
+                        else{
+                            tm.put(num, tm.get(num)-1);
+                        }
                     }
-                    cnt = tm.get(value);
-                    // delete if count == 1
-                    tm.remove(value);
-                    if(cnt > 1) tm.put(value, cnt-1);
                 }
             }
-            // print empty
-            if(tm.isEmpty()) sb.append("EMPTY\n");
-                // print max, min
+
+            if(tm.isEmpty()){
+                System.out.println("EMPTY");
+            }
             else{
-                sb.append(tm.lastKey()).append(" ").append(tm.firstKey()).append("\n");
+                System.out.println(tm.lastKey() + " " + tm.firstKey());
             }
         }
-        System.out.println(sb.toString());
     }
 }
