fb4ed4bcc670d9f77111e6d69b0d651660eed4c0
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-07-07 15:04:10 +0900

    [Silver I] Title: 영역 구하기, Time: 92 ms, Memory: 12180 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,93 +1,99 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int map[][];
-    static boolean vistied[][];
-    static int dx[] = {-1,0,1,0};
-    static int dy[] = {0,-1,0,1};
     static int N, M;
+    static int map[][];
+    static final int DRAW = 1;
+
+    static int dx[] = {-1, 0, 1, 0};
+    static int dy[] = {0, -1, 0, 1};
+    static boolean visited[][];
+
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-         M = Integer.parseInt(st.nextToken());
-         N = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
         map = new int[M][N];
-        vistied = new boolean[M][N];
+        visited = new boolean[M][N];  
+        int K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < k ; i++) {
+        for (int k = 0; k < K; k++) {
             st = new StringTokenizer(br.readLine());
-            int sx  = Integer.parseInt(st.nextToken());
-            int sy = Integer.parseInt(st.nextToken());
-            int ex = Integer.parseInt(st.nextToken());
-            int ey = Integer.parseInt(st.nextToken());
-
-            for (int x = sy ; x < ey ; x++) {
-                for (int y = sx ; y < ex ; y++) {
-                    map[x][y] =1;
-                }
-
-            }
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            initArray(x1, y1, x2, y2);
         }
-        LinkedList<Integer> result = new LinkedList<>();
-        int land = 0;
-        for (int i = 0; i <M ; i++) {
+
+        ArrayList<Integer> sizes = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if(map[i][j] != 1 && !vistied[i][j]){
-                    result.add(bfs(i,j));
-                    land ++;
+                if (map[i][j] != DRAW && !visited[i][j]) {
+                    sizes.add(bfs(i, j));
                 }
             }
         }
-        Collections.sort(result);
-        System.out.println(land);
-        for(int temp : result){
-            System.out.print(temp + " ");
+        Collections.sort(sizes);  
+
+        sb.append(sizes.size()).append("\n");
+        for (int size : sizes) {
+            sb.append(size).append(" ");
+        }
+        System.out.println(sb.toString().trim());  
+    }
+
+    public static void initArray(int x1, int y1, int x2, int y2) {
+        for (int i = y1; i < y2; i++) {
+            for (int j = x1; j < x2; j++) {
+                map[i][j] = DRAW;
+            }
         }
     }
 
-    private static int bfs(int si,int sj){
-        int cnt = 0;
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(si,sj));
-        vistied[si][sj] = true;
-        cnt ++;
+    public static int bfs(int i, int j) {
+        int size = 0;
+        Queue<Node> q = new LinkedList<>();
+        visited[i][j] = true;
+        q.add(new Node(i, j));
+        size++;  
 
-        while(!q.isEmpty()){
-            Point now = q.poll();
-            for (int d = 0; d < dx.length ; d++) {
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+            for (int d = 0; d < 4; d++) {
                 int nx = now.x + dx[d];
                 int ny = now.y + dy[d];
-
-                if(nx >= 0 && nx < M && ny >= 0 && ny < N && map[nx][ny] == 0 &&!vistied[nx][ny]){
-                    cnt++;
-                    vistied[nx][ny] = true;
-                    q.add(new Point(nx, ny));
-
+                if (isPossible(nx, ny)) {
+                    visited[nx][ny] = true;
+                    q.add(new Node(nx, ny));
+                    size++;
                 }
             }
         }
-        return cnt;
+        return size;
     }
 
-    private static void print(){
-        for (int i = 0; i <map.length; i++) {
-            for (int j = 0; j < map[0].length ; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("-------------------------");
+    public static boolean isPossible(int nx, int ny) {
+        return nx >= 0 && nx < M && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] != DRAW;
     }
-
 }
