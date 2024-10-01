b25dfc14fc0596d4c522a9d15515f45dd360875e
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-10-01 14:35:27 +0900

    [level 2] Title: 튜플, Time: 79.10 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,46 +1,20 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
+        Set<String> set = new HashSet<>();
         
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+                
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
         
-        s= s.replace("{","");
-        s=s.replace("},","-");
-        s=s.replace("}}","");
+        int[] answer = new int[arr.length];
         
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        String [] array = s.split("-"); // 배열에 한 번에 넣기 
-        
-        // array 정렬이 필요함 길이에 맞게 정렬 -> 2, 21, 213, 2134 => 2 1 3 4 답이 나옴 
-        Arrays.sort(array, new Comparator<String>(){
-            public int compare(String o1, String o2){
-                return Integer.compare(o1.length(), o2.length());
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
-        });
-        
-        for(int i = 0; i < array.length; i++){
-            String [] now = array[i].split(",");
-            int size = now.length;
-            for(int j = 0; j < size; j++){
-                int nowNum = Integer.parseInt(now[j]);
-                if(!list.contains(nowNum)){ // 해당 요소가 없다면 
-                    list.add(nowNum);
-                }
-            }
-     
         }
-        
-        int listSize = list.size();
-        
-        int answer[] = new int[listSize];
-        
-        for(int i = 0; i < listSize; i ++){
-            answer[i] = list.get(i);
-        }
-        
-        
-        
         return answer;
     }
 }
