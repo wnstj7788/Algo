3703695f257185ff9a667ba944098a42bbe1d806
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-10-02 09:19:38 +0900

    [level 3] Title: 징검다리 건너기, Time: 64.26 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 02일 09:19:21
+2024년 10월 02일 09:19:35
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,37 +1,6 @@
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
-        
-        int min =  1;
-        int max = 200000000;
-        
-        while(min <= max){
-            int mid = (min + max) / 2 ;
-            // 넘어갈 수 있다면 
-            if(isPossible(stones, k, mid)){
-                min = mid +1 ;
-                answer = Math.max(answer, mid);
-            }else{
-                // 못 넘어간다면 
-                max = mid - 1;
-            }
-            
-        }
         return answer;
-    }
-    
-    public static boolean isPossible(int[] stones, int k, int frineds){
-        int jump = 0;
-        for(int stone : stones){
-            if(stone - frineds < 0){
-                jump++;
-            }else{
-                jump = 0;
-            }
-            if(jump == k){
-                return false;
-            }
-        }
-        return true;
     }
 }
