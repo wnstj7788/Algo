10c2bf3554f050785cf01bd6c627c3a375727d84
최준서 <61938768+wnstj7788@users.noreply.github.com>
2024-10-03 15:22:06 +0900

    [level 3] Title: 표 편집, Time: 394.16 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,64 +1,53 @@
 import java.util.*;
 
 class Solution {
-    static class Node {
-        int prev, next;
-
-        public Node(int prev, int next) {
-            this.prev = prev;
+    static class Node{
+        int pre;
+        int next;
+        public Node(int pre, int next){
+            this.pre = pre;
             this.next = next;
         }
     }
-
     public String solution(int n, int k, String[] cmd) {
-        Node[] nodes = new Node[n];  // 각 노드의 prev, next 저장
-        Stack<Integer> removed = new Stack<>();  // 삭제된 행 저장
-        boolean[] isDeleted = new boolean[n];  // 삭제 여부 저장
-        
-        // 이중 연결 리스트 초기화
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(i - 1, i + 1);
+        String answer = "";
+        Stack<Integer> delNode = new Stack<>();
+        boolean[] isDel = new boolean[n];
+        Node[] node = new Node[n]; 
+        for(int i = 0; i < n; i++){
+            node[i] = new Node(i-1, i +1);
         }
-        nodes[n - 1].next = -1;  // 마지막 노드는 next가 없음
-
-        int current = k;  // 현재 선택된 행
-
-        for (String command : cmd) {
-            char c = command.charAt(0);
-
-            if (c == 'U') {
-                int move = Integer.parseInt(command.substring(2));
-                while (move-- > 0) current = nodes[current].prev;
-
-            } else if (c == 'D') {
-                int move = Integer.parseInt(command.substring(2));
-                while (move-- > 0) current = nodes[current].next;
-
-            } else if (c == 'C') {
-                removed.push(current);  // 삭제된 행을 스택에 저장
-                isDeleted[current] = true;
-
-                // 현재 노드 삭제
-                if (nodes[current].prev != -1) nodes[nodes[current].prev].next = nodes[current].next;
-                if (nodes[current].next != -1) nodes[nodes[current].next].prev = nodes[current].prev;
-
-                // 다음 노드로 이동 (마지막 행이면 이전 노드로)
-                current = (nodes[current].next != -1) ? nodes[current].next : nodes[current].prev;
-
-            } else if (c == 'Z') {
-                int restored = removed.pop();  // 최근 삭제된 행 복구
-                isDeleted[restored] = false;
-
-                // 복구된 노드 다시 연결
-                if (nodes[restored].prev != -1) nodes[nodes[restored].prev].next = restored;
-                if (nodes[restored].next != -1) nodes[nodes[restored].next].prev = restored;
+        node[n-1].next = -1; // 마지막 node 처리
+        
+        int nowIndex = k;
+        
+        for(String c : cmd){
+            char command = c.charAt(0);
+            if(command == 'U'){
+                int cnt = Integer.parseInt(c.substring(2));
+                while(cnt-- > 0) nowIndex = node[nowIndex].pre;
+            }else if(command == 'D'){
+                int cnt = Integer.parseInt(c.substring(2)); 
+                while(cnt-- > 0) nowIndex = node[nowIndex].next;
+            }else if(command == 'C'){
+                delNode.push(nowIndex);
+                isDel[nowIndex] = true;
+                if(node[nowIndex].pre != -1) node[node[nowIndex].pre].next = node[nowIndex].next;
+                if(node[nowIndex].next != -1) node[node[nowIndex].next].pre = node[nowIndex].pre;
+                nowIndex = (node[nowIndex].next == -1) ? node[nowIndex].pre : node[nowIndex].next;
+            }else if(command == 'Z'){
+                int undoIndex = delNode.pop();
+                isDel[undoIndex] = false;
+                if(node[undoIndex].pre != -1) node[node[undoIndex].pre].next = undoIndex;
+                if(node[undoIndex].next != -1) node[node[undoIndex].next].pre = undoIndex;
+                
             }
         }
-
+        
         // 결과 빌드
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            if (isDeleted[i]) result.append('X');
+            if (isDel[i]) result.append('X');
             else result.append('O');
         }
         
